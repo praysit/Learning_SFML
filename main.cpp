b50f63176bbbcb5cd26de0f5375ccfadfec6afe9
praysit <praysitsilwal2@outlook.com>
2026-07-10 21:51:09 +0100

    more smooth

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    constexpr unsigned int windowWidth = 800;
-    constexpr unsigned int windowHeight = 600;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Hello");
 
-    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "Hello");
-
-    sf::CircleShape circle;
-    circle.setRadius(150);
+    sf::CircleShape circle(150.f);
     circle.setOutlineColor(sf::Color::Black);
-    circle.setOutlineThickness(5);
+    circle.setOutlineThickness(5.f);
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -18,13 +14,15 @@ int main() {
             }
         }
 
-            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-            circle.setPosition({mousePos.x, mousePos.y});
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-                    window.clear(sf::Color::White);
-                    window.draw(circle);
-                    window.display();
-            }
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        circle.setPosition({static_cast<float>(mousePos.x), static_cast<float>(mousePos.y)});
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+            window.clear(sf::Color::White);
+            window.draw(circle);
+            window.display();
+        }
+
     }
     return 0;
 }

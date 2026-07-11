9f29fca281c64855b1a0193c98b3307da3edbe49
praysit <praysitsilwal2@outlook.com>
2026-07-11 16:55:04 +0100

    working gravity acting on a particle

--- main.cpp
+++ main.cpp
@@ -1,27 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Hello");
+    // init window
+    constexpr unsigned int windowWidth = 800;
+    constexpr unsigned int windowHeight = 600;
+    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "Gravity simulation");
 
-    sf::CircleShape circle(150.f);
+    // circle (particle)
+    sf::CircleShape circle(15.f);
     circle.setOutlineColor(sf::Color::Black);
-    circle.setOutlineThickness(5.f);
+    circle.setFillColor(sf::Color::Black);
+    circle.setPosition({400, 100});
 
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+
+    sf::Clock clock; // create clock
     while (window.isOpen()) {
+        sf::Time elapsed = clock.restart();
+        float deltaTime = elapsed.asSeconds(); // amount of time that passed between the last frame and the current one
+        const float gravity = 9.81f;
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
 
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        circle.setPosition({static_cast<float>(mousePos.x), static_cast<float>(mousePos.y)});
+        sf::Vector2f& pos = position; // reference (nickname) to position
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-            window.clear(sf::Color::White);
-            window.draw(circle);
-            window.display();
+        velocity.y += gravity * deltaTime;
+        pos.y += velocity.y * deltaTime * 100.f; // 100.f is a scale factor for pixels
+
+        if (pos.y > 570.f) {
+            pos.y = 570.f; // snap to floor
+            velocity.y *= -0.8f; // bounce
+
+            // stop jittering if the bounce is weak
+            if (std::abs(velocity.y) < 2.0f) { // abs is absolute value -> cmath
+                velocity.y = 0.0f;
+            }
         }
+        pos.x = 400;
+
+        circle.setPosition(position);
+
+    window.clear(sf::Color::White);
+    window.draw(circle);
+    window.display();
 
     }
     return 0;
